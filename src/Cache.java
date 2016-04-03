fc872f1ae528d18c96813c8ca963488442ff91d9
Chaitanya Malaviya <chaitanya@ChaitanyasMacbookAir.local>
2016-04-03 14:37:50 +0800

    More

--- src/Cache.java
+++ src/Cache.java
@@ -1,12 +1,12 @@
 import java.util.Date;
 
 public class Cache {
-	public String path;
-	public String data;
+	public String path; //File path
+	public String data; //Data read after read request
 	public int offset = -1;
 	public int length = -1;
-	public Date Tc;
-	public Date Tmclient;
+	public Date Tc; //Time when client last performed callback for updating cache 
+	public Date Tmclient;//Last modification time of file at client
 
 	public Cache(String filePath, String data, int offset, int length) {
 		path = filePath;
